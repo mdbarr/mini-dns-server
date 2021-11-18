f0eb1ba7fe99d37444136a88d30f39b3a49a71e8
Mark Barr <mark@hyperingenuity.com>
2021-11-18 12:24:47 -0500

    chore: dependency updates, linting rules update, automatic fixes

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const defaults = {
     allowDynamicZoneCreation: true,
     defaultTTL: 300,
     forwardZoneUnknowns: true,
-    silent: false
+    silent: false,
   },
   // DNS Server config
   dns: {
     port: 53,
-    host: '127.0.0.1', //'0.0.0.0',
+    host: '127.0.0.1', // '0.0.0.0',
     // Default nameservers
     nameservers: [
       '8.8.8.8',
-      '8.8.4.4'
+      '8.8.4.4',
     ],
     // RFC-6761 reserves four TLDs for special use:
     //   .example, .invalid, .localhost, and .test
@@ -25,44 +25,44 @@ const defaults = {
       test: {
         name: 'test',
         users: [ 'system', 'minidns' ],
-        SOA: [ [ 'test', {
-          name: 'test',
-          primary: 'ns.test',
-          admin: 'dns-admin.test',
-          refresh: 900,
-          retry: 900,
-          minimum: 60,
-          serial: 0,
-          expiration: 1800
-        } ] ],
-        A: [ [ 'localhost', {
-          address: '127.0.0.1'
-        } ] ],
-        AAAA: [ [ 'localhost', {
-          address: '::1'
-        } ] ],
-        CNAME: [ [ 'local', {
-          data: 'localhost.test'
-        } ] ],
-        MX: [ [ 'test', {
-          priority: 10,
-          exchange: 'mx.test'
-        } ] ],
-        TXT: [ [ 'test', {
-          data: [ `v=mini-dns-server v${ version }` ]
-        } ] ]
-      }
-    }
+        SOA: [
+          [
+            'test', {
+              name: 'test',
+              primary: 'ns.test',
+              admin: 'dns-admin.test',
+              refresh: 900,
+              retry: 900,
+              minimum: 60,
+              serial: 0,
+              expiration: 1800,
+            },
+          ],
+        ],
+        A: [ [ 'localhost', { address: '127.0.0.1' } ] ],
+        AAAA: [ [ 'localhost', { address: '::1' } ] ],
+        CNAME: [ [ 'local', { data: 'localhost.test' } ] ],
+        MX: [
+          [
+            'test', {
+              priority: 10,
+              exchange: 'mx.test',
+            },
+          ],
+        ],
+        TXT: [ [ 'test', { data: [ `v=mini-dns-server v${ version }` ] } ] ],
+      },
+    },
   },
   // API Server config
   api: {
     enabled: true,
     requireAuthorization: true,
-    version: version,
+    version,
     host: '0.0.0.0',
     port: 5354,
     customer: 'system',
-    key: 'mini-dns-8dj38#A65*5jdsP'
+    key: 'mini-dns-8dj38#A65*5jdsP',
   },
   dyn: {
     enabled: true,
@@ -71,16 +71,18 @@ const defaults = {
     host: '0.0.0.0',
     port: 5353,
     hostsFileSync: true,
-    customers: [ {
-      customer: 'minidns',
-      username: 'user',
-      password: 'password',
-      zones: [ 'test' ]
-    } ]
-  }
+    customers: [
+      {
+        customer: 'minidns',
+        username: 'user',
+        password: 'password',
+        zones: [ 'test' ],
+      },
+    ],
+  },
 };
 
-function MiniDns(options = {}) {
+function MiniDns (options = {}) {
   const minidns = this;
 
   minidns.util = require('./lib/util');
